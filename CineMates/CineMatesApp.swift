5c3699d7129fa47340329868dfa911e1406ce3f7
marco-agizza <marco.agizza@outlook.com>
2023-05-11 10:53:36 +0200

    Initial Commit

---
+++ CineMates/CineMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineMatesApp.swift
+//  CineMates
+//
+//  Created by Marco Agizza on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CineMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
